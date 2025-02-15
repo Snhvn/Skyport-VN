216e5832bc017db8da664aedc5c9c61109f84441
Snhvn <snhvnnguyen@gmail.com>
2025-02-15 17:50:31 +0700

    Update install-images-latest.sh

--- install-images-latest.sh
+++ install-images-latest.sh
@@ -1,1 +1,11 @@
-#!bin/bash
+#!/bin/bash
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Lưu ý vào chế độ ubuntu rồi cài"
+echo "Lưu ý tên folder vẫn y như v"
+echo "Đợi 3s để setup"
+sleep 3
+
+

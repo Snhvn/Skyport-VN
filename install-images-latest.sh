9c10722da74d4eae90b22c3f2e11477957d3b982
Snhvn <snhvnnguyen@gmail.com>
2025-02-15 17:55:31 +0700

    Update install-images-latest.sh

--- install-images-latest.sh
+++ install-images-latest.sh
@@ -7,5 +7,15 @@ echo "Lưu ý vào chế độ ubuntu rồi cài"
 echo "Lưu ý tên folder vẫn y như v"
 echo "Đợi 3s để setup"
 sleep 3
-
-
+clear
+cd panel5/exec
+rm -rf seed.js
+apt install wget -y
+wget https://raw.githubusercontent.com/Snhvn/Skyport-VN/refs/heads/main/panel5/exec/seed.js
+cd
+clear
+echo "------------------------"
+echo "SNIPA VN"
+echo "YouTube : https://youtube.com/@snipavn205"
+echo "------------------------"
+echo "Đã Cài xong vui lòng tắt panel
